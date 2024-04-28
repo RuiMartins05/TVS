7f1599aae784da62ce4b579f73f97ae156a57cdb
Nuno Palma <nunopalma98@gmail.com>
2024-04-28 20:57:22 +0100

    Added sample Question integration

--- src/main/java/pt/ulisboa/tecnico/STV/Question.java
+++ src/main/java/pt/ulisboa/tecnico/STV/Question.java
@@ -2,32 +2,66 @@ package pt.ulisboa.tecnico.STV;
 
 import pt.ulisboa.tecnico.STV.exception.InvalidOperationException;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Question {
+    private String body;
+    private List<String> choices;
+    private int correctChoice;
+    private List<String> topics;
+    private int weight;
+
     public Question(String body, List<String> choices, int correctChoice, String topic, int weight) throws
-            InvalidOperationException { /* .... */ }
+            InvalidOperationException {
+        !body.isEmpty() && 2 <= choices.size() <= 8 && 0 <= correctChoice <= (choices.size() - 1)
+                && 1 <= topics.size() <= 5 && 1 <= weight <= 15
+
+        this.body = body;
+        this.choices = new ArrayList<>(choices);
+        this.correctChoice = correctChoice;
+        this.topics = new ArrayList<>();
+        topics.add(topic);
+        this.weight = weight;
+    }
     // Removes a topic
-    public void remove(String topic) throws InvalidOperationException { /* .... */ }
+    public void remove(String topic) throws InvalidOperationException {
+        if (!topics.contains(topic))
+            throw new InvalidOperationException("Topic does not exist.");
+
+        topics.remove(topic);
+    }
     // Adds a new topic
-    public void add(String topic) throws InvalidOperationException { /* .... */ }
+    public void add(String topic) throws InvalidOperationException {
+        if (topics.contains(topic))
+            throw new InvalidOperationException("Topic already exists.");
+
+        topics.add(topic);
+    }
     // Returns all topics of this question
     public List<String> getTopics() {
-        return Collections.emptyList(); // For compilation purposes
+        return new ArrayList<>(topics);
     }
+
     // Computes the grade considering the weight of this question and the selected choice
     public float grade(int selectedChoice) {
         return 0.0f; // For compilation purposes
     }
     // Changes the weight of this question
-    public void setWeight(int weight) throws InvalidOperationException { /* .... */ }
+    public void setWeight(int weight) throws InvalidOperationException {
+        if (weight <= 0 || weight >= 16) {
+            throw new InvalidOperationException("Weight must be between 1 and 15, inclusive.");
+        }
+        this.weight = weight;
+    }
+
     // Returns the weight of this question
     public int getWeight() {
-        return 1; // For compilation purposes
+        return weight;
     }
+
     // Returns the choices of this question
     public List<String> getChoices() {
-        return Collections.emptyList(); // For compilation purposes
+        return choices;
     }
 }
